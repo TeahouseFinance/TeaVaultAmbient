3bba566ec3130a04cbed9fd9561ad334079e78e2
Ping-Che Chen <pcchen@kimicat.com>
2024-08-30 16:06:32 +0800

    Add prelimenary tests for user functions.

--- test/TeaVaultAmbient.js
+++ test/TeaVaultAmbient.js
@@ -4,6 +4,8 @@ const { expect } = require("chai");
 const { ethers, upgrades } = require("hardhat");
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const UINT256_MAX = '0x' + 'f'.repeat(64);
+const UINT64_MAX = '0x' + 'f'.repeat(16);
 
 function loadEnvVar(env, errorMsg) {
     if (env == undefined) {
@@ -28,6 +30,21 @@ async function getDecimals(token) {
     }
     else {
         return token.decimals();
+    }
+}
+
+async function approveToken(token, target, amount) {
+    if (token.target != ZERO_ADDRESS) {
+        return token.approve(target, amount);
+    }
+}
+
+async function getTokenBalance(token, target) {
+    if (token.target == ZERO_ADDRESS) {
+        return ethers.provider.getBalance(target);
+    }
+    else {
+        return token.balanceOf(target);
     }
 }
 
@@ -214,5 +231,106 @@ describe("TeaVaultAmbient", function () {
             .to.be.revertedWithCustomError(vault, "OwnableUnauthorizedAccount");
             expect(await vault.manager()).to.equal(manager.address);
         });
+    });
+
+    describe("User functions", function() {        
+        it("Should be able to deposit and withdraw from user", async function() {
+            const { owner, treasury, user, vault, token0 } = await helpers.loadFixture(deployTeaVaultAmbientFixture);
+
+            // set fees
+            const feeConfig = {
+                treasury: treasury.address,
+                entryFee: 1000,
+                exitFee: 2000,
+                performanceFee: 100000,
+                managementFee: 10000,
+            }
+
+            await vault.setFeeConfig(feeConfig);
+
+            // deposit
+            const token0Decimals = await getDecimals(token0);
+            const vaultDecimals = await getDecimals(vault);
+            await approveToken(token0, vault, ethers.parseUnits("10000", token0Decimals));
+            const shares = ethers.parseUnits("100", vaultDecimals);
+            const token0Amount = ethers.parseUnits("100", token0Decimals);
+
+            // let token0Before = await getTokenBalance(token0, user);
+            // if (token0 == ZERO_ADDRESS) {
+            //     // deposit native token
+            //     expect(await vault.connect(user).deposit(shares, UINT256_MAX, UINT256_MAX, { value: token0Amount }))
+            //     .to.changeTokenBalance(vault, user, shares);    
+            // }
+            // else {
+            //     // deposit ERC20 token
+            //     expect(await vault.connect(user).deposit(shares, UINT256_MAX, UINT256_MAX))
+            //     .to.changeTokenBalance(vault, user, shares);    
+            // }
+            // let token0After = await getBalance(token0, user);
+            // expect(token0Before - token0After).to.equal(token0Amount);
+
+            // let expectedAmount0 = ethers.BigNumber.from(token0Amount);
+            // const entryFeeAmount0 = expectedAmount0.mul(feeConfig.entryFee).div("1000000");
+            // expectedAmount0 = expectedAmount0.add(entryFeeAmount0);
+            // expect(token0Before.sub(token0After)).to.equal(expectedAmount0); // user spent expectedAmount0 of token0
+            // expect(await token0.balanceOf(owner.address)).to.equal(entryFeeAmount0); // vault received entryFeeAmount0 of token0
+            // const depositTime = await vault.lastCollectManagementFee();
+
+            // // withdraw
+            // token0Before = await token0.balanceOf(user.address);
+            // await vault.connect(user).withdraw(shares, 0, 0);
+            // expect(await vault.balanceOf(user.address)).to.equal(0);
+            // token0After = await token0.balanceOf(user.address);
+
+            // const withdrawTime = await vault.lastCollectManagementFee();
+            // const managementFeeTimeDiff = feeConfig.managementFee * (withdrawTime - depositTime);
+            // const feeMultiplier = await vault.FEE_MULTIPLIER();
+            // const secondsInAYear = await vault.SECONDS_IN_A_YEAR();
+            // const denominator = feeMultiplier * secondsInAYear - managementFeeTimeDiff;
+            // const managementFee = ethers.BigNumber.from(shares).mul(managementFeeTimeDiff).add(denominator - 1).div(denominator);
+
+            // const totalSupply = await vault.totalSupply();
+            // expectedAmount0 = ethers.BigNumber.from(token0Amount).mul(totalSupply.sub(managementFee)).div(totalSupply);
+            // const exitFeeAmount0 = expectedAmount0.mul(feeConfig.exitFee).div("1000000");
+            // const exitFeeShares = ethers.BigNumber.from(shares).mul(feeConfig.exitFee).div("1000000");
+            // expectedAmount0 = expectedAmount0.sub(exitFeeAmount0);
+            // expect(token0After.sub(token0Before)).to.be.closeTo(expectedAmount0, 100); // user received expectedAmount0 of token0
+            // expect(await vault.balanceOf(owner.address)).to.equal(exitFeeShares.add(managementFee)); // vault received exitFeeShares and managementFee of share
+        });
+
+        // it("Should not be able to deposit and withdraw incorrect amounts", async function() {
+        //     const { user, vault, token0 } = await helpers.loadFixture(deployTeaVaultV3Pair);
+
+        //     // deposit without enough allowance
+        //     await token0.connect(user).approve(vault.address, "1000" + "0".repeat(await token0.decimals()));
+        //     const shares = "10000" + "0".repeat(await vault.decimals());
+        //     await expect(vault.connect(user).deposit(shares, UINT256_MAX, UINT256_MAX)).to.be.revertedWith("");
+
+        //     const smallerShares = "100" + "0".repeat(await vault.decimals());
+        //     await vault.connect(user).deposit(smallerShares, UINT256_MAX, UINT256_MAX);
+
+        //     // withdraw more than owned shares
+        //     await expect(vault.connect(user).withdraw(shares, 0, 0)).to.be.revertedWith("");
+        // });
+
+        // it("Should revert with slippage checks when depositing", async function() {
+        //     const { user, vault, token0 } = await helpers.loadFixture(deployTeaVaultV3Pair);
+
+        //     // deposit with slippage check
+        //     await token0.connect(user).approve(vault.address, "10000" + "0".repeat(await token0.decimals()));
+        //     const shares = "10000" + "0".repeat(await vault.decimals());
+        //     await expect(vault.connect(user).deposit(shares, "100", "100")).to.be.revertedWith("");
+        // });
+
+        // it("Should revert with slippage checks when withdrawing", async function() {
+        //     const { user, vault, token0 } = await helpers.loadFixture(deployTeaVaultV3Pair);
+
+        //     await token0.connect(user).approve(vault.address, "1000" + "0".repeat(await token0.decimals()));
+        //     const shares = "100" + "0".repeat(await vault.decimals());
+        //     await vault.connect(user).deposit(shares, UINT256_MAX, UINT256_MAX);
+
+        //     // withdraw with slippage check
+        //     await expect(vault.connect(user).withdraw(shares, "100", "100")).to.be.revertedWith("");
+        // });
     });
 });
