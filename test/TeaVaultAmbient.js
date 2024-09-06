5287b6a26dd178619626a52b52744e18e35181be
Ping-Che Chen <pcchen@kimicat.com>
2024-09-06 17:51:36 +0800

    Update unit test script to add manager function test with ERC20 tokens.

--- test/TeaVaultAmbient.js
+++ test/TeaVaultAmbient.js
@@ -539,9 +539,17 @@ describe("TeaVaultAmbient", function () {
                 ]
             );
 
-            await token1Native.connect(user).approve(crocSwapDex, swapAmount2);
-            const outAmount2Data = await crocSwapDex.connect(user).userCmd.staticCall(1, callData);
-            const outAmount2 = abiCoder.decode([ "int128", "int128" ], outAmount2Data);
+            const crocImpact = await ethers.getContractAt("ICrocImpact", testImpact);
+            const outAmount2 = await crocImpact.calcImpact(
+                ZERO_ADDRESS,
+                token1Native.target,
+                testPoolIndex,
+                false,  // sell
+                false,  // in quote quantity
+                swapAmount2,
+                0n,
+                0n
+            );
             const swapCallData = crocSwapDex.interface.encodeFunctionData("userCmd", [ 1, callData ]);
             await vaultNative.connect(manager).executeSwap(false, swapAmount2, -outAmount2[0], crocSwapDex, swapCallData);
 
@@ -693,5 +701,208 @@ describe("TeaVaultAmbient", function () {
             await expect(vaultERC20.connect(user).withdraw(shares, token0Amount + 100n, 0n))
             .to.be.revertedWithCustomError(vaultERC20, "InvalidPriceSlippage");
         });
-    });    
+    });
+
+    describe("Manager functions with ERC20 tokens", function() {        
+        it("Should be able to swap, add liquidity, remove liquidity, and withdraw", async function() {
+            const { treasury, user, manager, vaultERC20, token0ERC20, token1ERC20 } = await helpers.loadFixture(deployTeaVaultAmbientFixture);
+
+            // set fees
+            const feeConfig = {
+                treasury: treasury.address,
+                entryFee: 1000n,
+                exitFee: 2000n,
+                performanceFee: 100000n,
+                managementFee: 0n,          // leave management fee at zero to make sure the vault can be emptied
+            }
+
+            await vaultERC20.setFeeConfig(feeConfig);
+
+            const feeMultiplier = await vaultERC20.FEE_MULTIPLIER();
+
+            // deposit
+            const token0Decimals = await token0ERC20.decimals();
+            const vaultDecimals = await vaultERC20.decimals();
+            const shares = ethers.parseUnits("1", vaultDecimals);
+            const token0Amount = ethers.parseUnits("1", token0Decimals);
+            const token0EntryFee = token0Amount * feeConfig.entryFee / feeMultiplier;
+            const token0AmountWithFee = token0Amount + token0EntryFee;
+
+            await token0ERC20.connect(user).approve(vaultERC20, token0AmountWithFee);
+            await vaultERC20.connect(user).deposit(shares, token0AmountWithFee, 0n);
+
+            // manager swap, using UniswapV3
+            const v3Router = new ethers.Contract(testRouter, UniswapV3SwapRouterABI, ethers.provider);
+            const swapAmount = token0Amount / 2n;
+            const swapRelayer = await vaultERC20.swapRelayer();
+            const swapParams = [
+                token0ERC20.target,
+                token1ERC20.target,
+                500,
+                swapRelayer,
+                UINT64_MAX,
+                swapAmount,
+                0n,
+                0n
+            ];
+            await token0ERC20.connect(user).approve(v3Router, swapAmount);
+            const outAmount = await v3Router.connect(user).exactInputSingle.staticCall(swapParams);
+            const uniswapV3SwapData = v3Router.interface.encodeFunctionData("exactInputSingle", [ swapParams ]);
+            await vaultERC20.connect(manager).executeSwap(true, swapAmount, outAmount, v3Router.target, uniswapV3SwapData);
+
+            const amount0AfterSwap = await token0ERC20.balanceOf(vaultERC20);
+            const amount1AfterSwap = await token1ERC20.balanceOf(vaultERC20);
+            expect(amount0AfterSwap).to.gte(token0Amount - swapAmount); // should use swapAmount or less
+            expect(amount1AfterSwap).to.gte(outAmount); // should receive outAmount or more
+
+            // add liquidity
+            const poolInfo = await vaultERC20.getPoolInfo();
+            const currentTick = poolInfo[7];
+            const tickSpacing = poolInfo[5];
+
+            // add positions
+            const tick0 = ((currentTick - tickSpacing * 30n) / tickSpacing) * tickSpacing;
+            const tick1 = ((currentTick - tickSpacing * 10n) / tickSpacing) * tickSpacing;
+            const tick2 = ((currentTick + tickSpacing * 10n) / tickSpacing) * tickSpacing;
+            const tick3 = ((currentTick + tickSpacing * 30n) / tickSpacing) * tickSpacing;
+
+            // add "center" position
+            let liquidity1 = await vaultERC20.getLiquidityForAmounts(tick1, tick2, amount0AfterSwap / 3n, amount1AfterSwap / 3n);
+            await vaultERC20.connect(manager).addLiquidity(tick1, tick2, liquidity1, 0, 0, UINT64_MAX);
+
+            let positionInfo = await vaultERC20.positionInfo(0);
+            let amounts = await vaultERC20.getAmountsForLiquidity(tick1, tick2, liquidity1);
+            expect(positionInfo[0]).to.be.closeTo(amounts[0], 1n);
+            expect(positionInfo[1]).to.be.closeTo(amounts[1], 1n);
+            
+            // add "lower" position
+            const amount0 = await token0ERC20.balanceOf(vaultERC20);
+            let liquidity0 = await vaultERC20.getLiquidityForAmounts(tick0, tick1, amount0, 0);
+            await vaultERC20.connect(manager).addLiquidity(tick0, tick1, liquidity0, 0, 0, UINT64_MAX);
+
+            positionInfo = await vaultERC20.positionInfo(1);
+            amounts = await vaultERC20.getAmountsForLiquidity(tick0, tick1, liquidity0);
+            expect(positionInfo[0]).to.be.closeTo(amounts[0], 1n);
+            expect(positionInfo[1]).to.be.closeTo(amounts[1], 1n);
+
+            // add "upper" position
+            const amount1 = await token1ERC20.balanceOf(vaultERC20);
+            let liquidity2 = await vaultERC20.getLiquidityForAmounts(tick2, tick3, 0, amount1 - 10n); // slightly lower amount1 to avoid precision problem
+            await vaultERC20.connect(manager).addLiquidity(tick2, tick3, liquidity2, 0, 0, UINT64_MAX);
+
+            positionInfo = await vaultERC20.positionInfo(2);
+            amounts = await vaultERC20.getAmountsForLiquidity(tick2, tick3, liquidity2);
+            expect(positionInfo[0]).to.be.closeTo(amounts[0], 1n);
+            expect(positionInfo[1]).to.be.closeTo(amounts[1], 1n);
+
+            // check assets and token values
+            let assets = await vaultERC20.vaultAllUnderlyingAssets();
+            expect(assets[0]).to.be.closeTo(amount0AfterSwap, amount0AfterSwap / 100n);
+            expect(assets[1]).to.be.closeTo(amount1AfterSwap, amount1AfterSwap / 100n);
+
+            expect(await vaultERC20.estimatedValueInToken0()).to.be.closeTo(amount0AfterSwap * 2n, amount0AfterSwap * 2n / 100n);
+            expect(await vaultERC20.estimatedValueInToken1()).to.be.closeTo(amount1AfterSwap * 2n, amount1AfterSwap * 2n / 100n);
+
+            // add more liquidity
+            const shares2 = ethers.parseUnits("2", vaultDecimals);
+            const totalShares = await vaultERC20.totalSupply();
+            let token0Amount2 = (assets[0] * shares2 + totalShares - 1n) / totalShares;
+            let token1Amount2 = (assets[1] * shares2 + totalShares - 1n) / totalShares;
+            token0Amount2 += (token0Amount2 * feeConfig.entryFee + feeMultiplier - 1n) / feeMultiplier;
+            token1Amount2 += (token1Amount2 * feeConfig.entryFee + feeMultiplier - 1n) / feeMultiplier;
+
+            // deposit more
+            await token0ERC20.connect(user).approve(vaultERC20, token0Amount2);
+            await token1ERC20.connect(user).approve(vaultERC20, token1Amount2);
+            await vaultERC20.connect(user).deposit(shares2 * 99n/ 100n, token0Amount2, token1Amount2);
+
+            // reduce some position
+            await helpers.time.increase(1000);   // advance some time to get over the "JIT" limit
+            const position = await vaultERC20.positions(2);
+            await vaultERC20.connect(manager).removeLiquidity(position.tickLower, position.tickUpper, position.liquidity, 0, 0, UINT64_MAX);
+
+            // check assets and token values
+            assets = await vaultERC20.vaultAllUnderlyingAssets();
+            const newAmount0 = amount0AfterSwap + token0Amount2;
+            const newAmount1 = amount1AfterSwap + token1Amount2;
+            expect(assets[0]).to.be.closeTo(newAmount0, newAmount0 / 100n);
+            expect(assets[1]).to.be.closeTo(newAmount1, newAmount1 / 100n);
+
+            expect(await vaultERC20.estimatedValueInToken0()).to.be.closeTo(newAmount0 * 2n, newAmount0 * 2n / 100n);
+            expect(await vaultERC20.estimatedValueInToken1()).to.be.closeTo(newAmount1 * 2n, newAmount1 * 2n / 100n);
+
+            // manager swap back, using CrocSwapDex
+            const swapAmount2 = await token1ERC20.balanceOf(vaultERC20);
+            const crocSwapDex = await ethers.getContractAt("ICrocSwapDex", testSwapDex);
+            const abiCoder = ethers.AbiCoder.defaultAbiCoder();
+            const callData = abiCoder.encode(
+                [
+                    "address",
+                    "address",
+                    "uint256", 
+                    "bool", 
+                    "bool",
+                    "uint128",
+                    "uint16",
+                    "uint128",
+                    "uint128",
+                    "uint8"
+                ],
+                [
+                    token0ERC20.target,
+                    token1ERC20.target,
+                    testPoolIndex,
+                    false,  // sell
+                    false,  // in quote quantity
+                    swapAmount2,
+                    0n,
+                    0n,
+                    0n,
+                    0n
+                ]
+            );
+
+            const crocImpact = await ethers.getContractAt("ICrocImpact", testImpact);
+            const outAmount2 = await crocImpact.calcImpact(
+                token0ERC20.target,
+                token1ERC20.target,
+                testPoolIndex,
+                false,  // sell
+                false,  // in quote quantity
+                swapAmount2,
+                0n,
+                0n
+            );
+            const swapCallData = crocSwapDex.interface.encodeFunctionData("userCmd", [ 1, callData ]);
+            await vaultERC20.connect(manager).executeSwap(false, swapAmount2, -outAmount2[0], crocSwapDex, swapCallData);
+
+            // withdraw
+            const amount0Before = await token0ERC20.balanceOf(user);
+            const amount1Before = await token1ERC20.balanceOf(user);
+            const userShares = await vaultERC20.balanceOf(user);
+            expect(await vaultERC20.connect(user).withdraw(userShares, 0, 0))
+            .to.changeTokenBalance(vaultERC20, user, -userShares);
+            const amount0After = await token0ERC20.balanceOf(user);
+            const amount1After = await token1ERC20.balanceOf(user);
+
+            // estimate value of received tokens
+            const amount0Diff = amount0After - amount0Before;
+            const amount1Diff = amount1After - amount1Before;
+            const sqrtPriceQ64 = poolInfo[6];
+            const price = sqrtPriceQ64 * sqrtPriceQ64;
+            const totalIn0 = amount1Diff * price / (1n << 128n) + amount0Diff;
+
+            // expect withdrawn tokens to be > 95% of invested token0
+            const investedToken0 = token0AmountWithFee + token0Amount2 + token1Amount2 * price / (1n << 128n);
+            expect(totalIn0).to.be.closeTo(investedToken0, investedToken0 / 50n);
+
+            // remove the remaining share
+            const remainShares = await vaultERC20.balanceOf(treasury);
+            await vaultERC20.connect(treasury).withdraw(remainShares, 0, 0);
+            expect(await vaultERC20.totalSupply()).to.equal(0);
+
+            // positions should be empty
+            expect(await vaultERC20.getAllPositions()).to.eql([]);
+        });
+    });
 });
